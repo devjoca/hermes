2808e83634285f0f9a8d843f32c62000ae62f2f9
joca <jpereyraleon@gmail.com>
2019-03-15 14:03:59 -0500

    add login page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,17 +1,20 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
-  </head>
-  <body class="font-sans antialiased text-black leading-tight">
+</head>
+
+<body class="bg-grey-lighter h-screen font-sans">
     <div id="app">
-      @yield('body')
+        @yield('body')
     </div>
     <script src="{{ mix('js/app.js') }}"></script>
-  </body>
+</body>
+
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,28 @@
 @extends('layouts.app')
 
 @section('body')
-    <div class="min-h-screen flex items-center justify-center">
-        <h1 class="text-5xl text-purple font-sans">Greetings.</h1>
+<div class="container mx-auto h-full flex justify-center items-center">
+    <div class="w-1/3">
+        <h1 class="font-hairline mb-6 text-center">Welcome to Hermes</h1>
+            <form action="/login" method="POST">
+                {{ csrf_field() }}
+                <div class="border-teal p-8 border-t-12 bg-white mb-6 rounded-lg shadow-lg">
+                    <div class="mb-4">
+                        <label class="font-bold text-grey-darker block mb-2">Email</label>
+                        <input type="text" class="block appearance-none w-full bg-white border border-grey-light hover:border-grey px-2 py-2 rounded shadow" placeholder="Email">
+                    </div>
+
+                    <div class="mb-4">
+                        <label class="font-bold text-grey-darker block mb-2">Password</label>
+                        <input type="password" class="block appearance-none w-full bg-white border border-grey-light hover:border-grey px-2 py-2 rounded shadow" placeholder="Password">
+                    </div>
+
+                    <div class="flex items-center justify-between">
+                        <button class="bg-teal-dark hover:bg-teal text-white font-bold py-2 px-4 rounded">
+                            Login
+                        </button>
+                    </div>
+            </form>
     </div>
+</div>
 @endsection
