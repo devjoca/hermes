eb5d15dfad47ea1b991e3ef8ac610dc226348a1d
joca <jpereyraleon@gmail.com>
2019-03-15 15:06:21 -0500

    fresh style for login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,26 +3,30 @@
 @section('body')
 <div class="container mx-auto h-full flex justify-center items-center">
     <div class="w-1/3">
-        <h1 class="font-hairline mb-6 text-center">Welcome to Hermes</h1>
-            <form action="/login" method="POST">
-                {{ csrf_field() }}
-                <div class="border-teal p-8 border-t-12 bg-white mb-6 rounded-lg shadow-lg">
-                    <div class="mb-4">
-                        <label class="font-bold text-grey-darker block mb-2">Email</label>
-                        <input type="text" class="block appearance-none w-full bg-white border border-grey-light hover:border-grey px-2 py-2 rounded shadow" placeholder="Email">
-                    </div>
+        <h1 class="font-hairline mb-6 text-center">Hermes ⚡️</h1>
+        <form action="/login" method="POST">
+            {{ csrf_field() }}
+            <div class="border-teal p-8 border-t-12 bg-white mb-6 rounded-lg shadow-lg">
+                <div class="mb-4">
+                    <label class="block text-grey-darker text-sm font-bold mb-2" for="username">
+                        Username
+                    </label>
+                    <input class="shadow appearance-none border rounded w-full h-12 py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username">
+                </div>
 
-                    <div class="mb-4">
-                        <label class="font-bold text-grey-darker block mb-2">Password</label>
-                        <input type="password" class="block appearance-none w-full bg-white border border-grey-light hover:border-grey px-2 py-2 rounded shadow" placeholder="Password">
-                    </div>
+                <div class="mb-4">
+                    <label class="block text-grey-darker text-sm font-bold mb-2" for="password">
+                        Password
+                    </label>
+                    <input class="shadow appearance-none border rounded w-full h-12 py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="************">
+                </div>
 
-                    <div class="flex items-center justify-between">
-                        <button class="bg-teal-dark hover:bg-teal text-white font-bold py-2 px-4 rounded">
-                            Login
-                        </button>
-                    </div>
-            </form>
+                <div class="flex items-center justify-between">
+                    <button class="bg-green hover:bg-green-dark text-white font-bold w-full h-12 py-2 px-4  rounded focus:outline-none focus:shadow-outline" type="button">
+                        Log In
+                    </button>
+                </div>
+        </form>
     </div>
 </div>
 @endsection
