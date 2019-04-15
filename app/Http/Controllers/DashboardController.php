200be1a3ff52c220251e536c3e15ed202e994afe
joca <jpereyraleon@gmail.com>
2019-04-15 15:01:11 -0500

    show dashboard data

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Spatie\UptimeMonitor\Models\Monitor;
 
 class DashboardController extends Controller
 {
     public function __invoke()
     {
-        return view('dashboard.monitor');
+        $monitors = Monitor::all()->map(function($m) {
+            return [
+                'url' => "{$m->url}",
+                'status' => $m->uptime_status,
+                'certificate_check_enabled' => $m->certificate_check_enabled,
+                'certificate_status' => $m->certificate_status,
+            ];
+        });
+
+        return view('dashboard.monitor', compact('monitors'));
     }
 }
--- resources/views/dashboard/monitor.blade.php
+++ resources/views/dashboard/monitor.blade.php
@@ -1,29 +1,29 @@
 @extends('layouts.dashboard')
 
 @section('body')
-<div class="py-6 border-grey-light border-b">
+<div class="py-6 border-grey-light">
     <h1>Apps</h1>
 </div>
 <div>
     <table class="text-left w-full border-collapse bg-white text-center">
         <tr>
-            <th class="py-4 px-6 font-bold text-sm text-grey-dark border-b border-grey-light">ID</th>
-            <th class="py-4 px-6 font-bold text-sm text-grey-dark border-b border-grey-light">NAME </th>
+            <th class="py-4 px-6 font-bold text-sm text-grey-dark border-b border-grey-light">#</th>
             <th class="py-4 px-6 font-bold text-sm text-grey-dark border-b border-grey-light">URL</th>
-            <th class="py-4 px-6 font-bold text-sm text-grey-dark border-b border-grey-light">SSL?</th>
             <th class="py-4 px-6 font-bold text-sm text-grey-dark border-b border-grey-light">STATUS</th>
+            <th class="py-4 px-6 font-bold text-sm text-grey-dark border-b border-grey-light">SSL?</th>
             <th class="py-4 px-6 font-bold text-sm text-grey-dark border-b border-grey-light">ACTIONS</th>
         </tr>
+        @foreach($monitors as $monitor)
         <tr class="border-b">
-            <td>1</td>
-            <td>Orus Dev</td>
-            <td>http://orus.dev.andromeda.tektonlabs.com</td>
-            <td>✅</td>
-            <td>✅</td>
+            <td>{{ $loop->iteration }}</td>
+            <td><a href="{{ $monitor['url'] }}" target="_blank">{{ $monitor['url'] }}</a></td>
+            <td>{{ $monitor['status'] }}</td>
+            <td>{{ $monitor['certificate_check_enabled'] ? $monitor['certificate_status'] : 'not enabled' }}</td>
             <td>
                 Editar | Eliminar
             </td>
         </tr>
+        @endforeach
     </table>
 </div>
 @endsection
