1d99c843fcde2313e7383dcecbc67991e4d90743
joca <jpereyraleon@gmail.com>
2019-10-17 17:14:17 -0500

    install standard and lint action

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,7 +7,8 @@
 
 require('./bootstrap');
 
-window.Vue = require('vue');
+const Vue = require('vue');
+window.Vue = Vue;
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
